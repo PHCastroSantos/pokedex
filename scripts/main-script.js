c8ae2ffd38dc7791854c192d2c64bf2f4eb901b0
Pedro Castro <phcastroestudoss@gmail.com>
2024-03-11 22:39:01 -0300

    Adicionando fundo ao display da Pokédex

--- scripts/main-script.js
+++ scripts/main-script.js
@@ -1,15 +1,23 @@
 "use strict";
 
+
+
 (function turnOff() {
+
   let turnOnOffButton = document.querySelector(".under-display-button");
   let screen = document.querySelector(".content-display");
   screen.style.backgroundColor = "black";
 
   turnOnOffButton.addEventListener("click", function () {
     if (screen.style.backgroundColor === "black") {
+      screen.style.backgroundImage = "url('../src/bk-pokemon.png')";
+      screen.style.backgroundSize = "cover";
+      screen.style.backgroundRepeat = "no-repeat";
+      screen.style.backgroundPosition = "center";
       screen.style.backgroundColor = "white";
     } else {
       screen.style.backgroundColor = "black";
+      screen.style.backgroundImage = "none";
     }
   });
 })();
