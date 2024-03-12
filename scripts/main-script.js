abfc9416949c8a15501f80a9c40eb42af7b366d7
Pedro Castro <phcastroestudoss@gmail.com>
2024-03-11 22:52:30 -0300

    Adicionando algumas funcionalidades e pokémons

--- scripts/main-script.js
+++ scripts/main-script.js
@@ -6,6 +6,7 @@
 
   let turnOnOffButton = document.querySelector(".under-display-button");
   let screen = document.querySelector(".content-display");
+  let pokemon = document.querySelector(".pokemon")
   screen.style.backgroundColor = "black";
 
   turnOnOffButton.addEventListener("click", function () {
@@ -15,9 +16,11 @@
       screen.style.backgroundRepeat = "no-repeat";
       screen.style.backgroundPosition = "center";
       screen.style.backgroundColor = "white";
+      pokemon.style.display = "flex"
     } else {
       screen.style.backgroundColor = "black";
       screen.style.backgroundImage = "none";
+      pokemon.style.display = "none"
     }
   });
 })();
