db6f304d596fe01a8e74465a4c81ef7464f0cb92
Pedro Castro <phcastroestudoss@gmail.com>
2024-03-17 22:45:39 -0300

    Alterações de tamanho e media query

--- scripts/main-script.js
+++ scripts/main-script.js
@@ -22,10 +22,21 @@ const renderPokemon = async(pokemon) => {
   const data = await fetchPokemon(pokemon);
 
   pokemonName.innerHTML = data.name;
-  pokemonPic.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default']  
-  pokemonType1.innerHTML = data['types']['0']['type']['name']
-  pokemonType2.innerHTML = data['types']['1']['type']['name']
-} 
+  pokemonPic.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
+  pokemonType1.innerHTML = data['types']['0']['type']['name'];
+  pokemonType2.innerHTML = data['types']['1']['type']['name'];
+
+  if(pokemonType1.innerHTML = data['types']['0']['type']['name'] === "grass") {
+    pokemonType1.innerHTML = "<span style='width: 30px; height: 30px' class='game-icons--high-grass'></span>";
+  }
+
+  if(pokemonType2.innerHTML = data['types']['1']['type']['name'] === "poison") {
+    pokemonType2.innerHTML = "<span style='width: 30px; height: 30px' class='mdi--poison'></span>";
+  }
+
+  
+}
+
 
 renderPokemon('1');
 
@@ -53,7 +64,6 @@ renderPokemon('1');
       pokemonName.style.display = ""
       typeNames.style.display = ""
     }
-
 
   });
 })();
